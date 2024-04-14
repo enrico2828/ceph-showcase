0073bd7016f053625b5bdd9dbe08fdf36ee539bb
Enrico Welder <enwelder@gmail.com>
2024-04-14 20:24:38 +0200

    update objectstore multisite files

--- minikube/create-rook2-cluster.sh
+++ minikube/create-rook2-cluster.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 export ROOK_PROFILE_NAME=rook2
-export ROOK_OBJECTSTORE_SPEC_FILE=/home/ewelder/Git/ceph-showcase/minikube/deploy/objectstore/object-test.yaml
+export ROOK_OBJECTSTORE_SPEC_FILE=/home/ewelder/Git/ceph-showcase/minikube/deploy/objectstore/object-multisite-pull-test.yaml
 
 ./create-dev-cluster.sh -f -m -o
