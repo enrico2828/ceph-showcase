61ba638d0c09fb6f4ff3f7eee022d6c8c2cfc588
Enrico Welder <enwelder@gmail.com>
2024-04-21 15:43:55 +0200

    2 vagrant vms with cephadm

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -1,11 +1,19 @@
-
 #!/bin/env bash
 # This script is used to bootstrap a Ceph cluster
+
+IP=${1}
+
+if [ -z "${IP}" ]; then
+  echo "Usage: $0 <IP>"
+  exit 1
+fi
 
 sudo apt update
 sudo apt upgrade -y
 sudo apt install -y cephadm docker.io ceph-common net-tools
 
-cephadm bootstrap --mon-ip 192.168.50.10 --single-host-defaults 
+cephadm bootstrap --mon-ip "${IP}" --single-host-defaults 
 ceph orch apply osd --all-available-devices
 ceph orch apply rgw foo
+
+ceph dashboard set-prometheus-api-host http://"${IP}":9095
